a2f72ef57fffeef35dc1f6dd31e74b750004f779
LarSyMain <96264278+LarSyMain@users.noreply.github.com>
2024-02-09 10:56:36 +0300

    Add files via upload

---
+++ lab1(2)/GetPut.pas
@@ -0,0 +1,26 @@
+﻿unit GetPut;
+interface
+
+  uses Obr;
+  procedure Get(var x : Tset; f : TextFile);
+  
+implementation
+  
+procedure Get(var x : Tset; f : TextFile);
+var
+  s : string;
+  i, n : integer;
+  ch : Tset;
+begin
+  
+  readln(f, s);
+  n := length(s);
+  for i := 1 to n do
+  begin
+    x := x + [s[i]];    
+  end;
+  
+end;
+  
+
+end.
---
+++ lab1(2)/Obr.pas
@@ -0,0 +1,39 @@
+﻿unit Obr;
+
+interface
+
+type
+  Tset = set of char;
+procedure Ind(var x : Tset);
+function ob(var x : Tset; var y : Tset) : Tset;
+function per(var x : Tset; var y : Tset) : Tset;
+function raz(var x : Tset; var y : Tset) : Tset;
+function form(var x, y, z : Tset) : Tset;
+implementation
+
+    procedure Ind(var x : Tset);
+    begin
+      x := [];
+    end;
+    
+    function ob(var x : Tset; var y : Tset) : Tset;
+    begin
+      result := x + y;
+    end;
+    
+    function per(var x : Tset; var y : Tset) : Tset;
+    begin
+      result := x * y;
+    end;
+    
+    function raz(var x : Tset; var y : Tset) : Tset;
+    begin
+      result := x - y;
+    end;
+    
+    function form(var x, y, z : Tset) : Tset;
+    begin
+      result := (x-z)+(y-z);
+    end;
+    
+end.
